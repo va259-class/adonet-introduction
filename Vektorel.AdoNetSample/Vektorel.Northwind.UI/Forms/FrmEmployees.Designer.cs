30136ede0fba3b7428c0f7d3a3130b8eb68310c5
va259-class <va259@outlook.com>
2026-01-04 10:42:51 +0300

    Employees page step 1

---
+++ Vektorel.AdoNetSample/Vektorel.Northwind.UI/Forms/FrmEmployees.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Vektorel.Northwind.UI.Forms
+{
+    partial class FrmEmployees
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvEmployees = new DataGridView();
+            txtSearch = new TextBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvEmployees).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvEmployees
+            // 
+            dgvEmployees.AllowUserToAddRows = false;
+            dgvEmployees.AllowUserToDeleteRows = false;
+            dgvEmployees.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvEmployees.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEmployees.Location = new Point(12, 42);
+            dgvEmployees.Name = "dgvEmployees";
+            dgvEmployees.ReadOnly = true;
+            dgvEmployees.Size = new Size(510, 307);
+            dgvEmployees.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            txtSearch.Location = new Point(369, 12);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(153, 23);
+            txtSearch.TabIndex = 1;
+            txtSearch.TextChanged += txtSearch_TextChanged;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(338, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(25, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Ara";
+            // 
+            // FrmEmployees
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(534, 361);
+            Controls.Add(label1);
+            Controls.Add(txtSearch);
+            Controls.Add(dgvEmployees);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            MinimumSize = new Size(500, 400);
+            Name = "FrmEmployees";
+            Text = "Personel Listesi";
+            Load += FrmEmployees_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvEmployees).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvEmployees;
+        private TextBox txtSearch;
+        private Label label1;
+    }
+}
---
+++ Vektorel.AdoNetSample/Vektorel.Northwind.UI/Forms/FrmEmployees.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Configuration;
+
+namespace Vektorel.Northwind.UI.Forms
+{
+    public partial class FrmEmployees : Form
+    {
+        public FrmEmployees()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmEmployees_Load(object sender, EventArgs e)
+        {
+            LoadEmployees();
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            LoadEmployees(txtSearch.Text);
+        }
+
+        private void LoadEmployees(string filter = null)
+        {
+            using var connection = GetConnection();
+            var queryFilter = string.Empty;
+            if (!string.IsNullOrWhiteSpace(filter))
+            {
+                queryFilter = @$"where FirstName like '{txtSearch.Text}%' or 
+                                       LastName like '{txtSearch.Text}%' or
+	                                   Country like '{txtSearch.Text}%' or
+	                                   HomePhone like '%{txtSearch.Text}%'";    
+            }
+
+            var query = @$"select FirstName, LastName, Country, Title, HomePhone
+                           from Employees
+                           {queryFilter}";
+            using var command = new SqlCommand(query, connection);
+            using var reader = command.ExecuteReader();
+            var list = new List<EmployeeDTO>();
+            while (reader.Read())
+            {
+                var emp = new EmployeeDTO
+                {
+                    FirstName = reader[nameof(EmployeeDTO.FirstName)].ToString(),
+                };
+            }
+        }
+
+        private SqlConnection GetConnection()
+        {
+            var connectionString = ConfigurationManager.AppSettings.Get("ConnStr");
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            return connection;
+        }
+
+        class EmployeeDTO
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Country { get; set; }
+            public string Title { get; set; }
+            public string HomePhone { get; set; }
+        }
+    }
+}
